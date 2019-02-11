467295a5e2a573ea30e658f56240d709a7b37ffd
kels-orien <kelvinorien@gmail.com>
2019-02-11 22:15:57 +0100

    update UI components EditProfile-2

--- client/src/containers/EditProfile/index.js
+++ client/src/containers/EditProfile/index.js
@@ -6,7 +6,6 @@ import { SubmitButton, LinkButton } from "../../components/Button";
 import formstyle from "../../components/Form";
 import TextField from "@material-ui/core/TextField";
 import Card from "@material-ui/core/Card";
-import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
 import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core/styles";
